c2aec7e904f6497f326c5756267035bceb9d1409
ketan gupta <ketangupta34@gmail.com>
2021-03-13 12:41:00 +0530

    animatpoy application

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,13 @@ function Header() {
   return (
     <div className="header">
       <h1 className="headerLogo">Animatopy</h1>
-      <img
-        className="headerGithubLink"
-        src="https://img.icons8.com/fluent/48/000000/github.png"
-        alt="github link"
-      />
+      <a href="https://github.com/ketangupta34/animatopy">
+        <img
+          className="headerGithubLink"
+          src="https://img.icons8.com/fluent/48/000000/github.png"
+          alt="github link"
+        />
+      </a>
     </div>
   );
 }
